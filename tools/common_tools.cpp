9288e6d0d34fd929d70029a5614056564487675d
dma122 <dma122@csil-cpu03.csil.sfu.ca>
2024-10-16 20:23:57 -0700

    Added generic sorting functions for lists

--- tools/common_tools.cpp
+++ tools/common_tools.cpp
@@ -1,7 +1,22 @@
 #include <algorithm>
 #include <string>
+#include <vector>
 
 std::string removeWhiteSpace(std::string s){
     s.erase(std::remove(s.begin(), s.end(), ' '), s.end());
     return s;
+}
+
+//sorts in ascending order
+template <typename MapType>
+void sortList(std::vector <MapType>&list){
+    std::ranges::sort(list, std::less{});
+}
+
+//sorts in ascending order using a key
+template <typename MapType>
+void sortList(std::vector <MapType>&list, const auto &key){
+    std::ranges::sort(list, std::less{}, [&key](const MapType& mapElement) {
+        return mapElement.at(key);
+    });
 }
