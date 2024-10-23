0a80263848a7db0dc6ebbe0323493399577c10a6
Nicholas Tait <nicholastait15@gmail.com>
2024-10-23 13:45:07 -0700

    changed folder structure need to fix compilations erros within game directory
    nta52

--- game/include/Player.h
+++ game/include/Player.h
@@ -4,51 +4,33 @@
 #include "Client.h"
 #include "Server.h"
 #include <iostream>
-#include <string>
 #include <map>
+#include <string>
 #include <vector>
 using namespace std;
 
 template <typename T, typename M> // M is for player, T is for audience
-class Player{
-    public:
-        Player();
-<<<<<<< HEAD
-        Player(networking::Connection, int ID, T audienceVar, M playerVar);
-        ~Player();
-        void make_choice(const string&);
-        networking::Connection getConnection();
-        // int roundGetter();
-        string getChoice() const;
-        int getId() const;
+class Player {
+public:
+  Player();
+  Player(networking::Connection, int ID, T audienceVar, M playerVar);
+  ~Player();
+  void make_choice(const string &);
+  networking::Connection getConnection();
+  // int roundGetter();
+  string getChoice() const;
+  int getId() const;
 
-        bool inAudience();
-        GamePerPlayer<M>& getPerPlayer();
-        GamePerAudience<T>& getPerAudience();
-    
-    private:
-        networking::Connection connection;
-        string choice;
-        int id;
+  bool inAudience();
+  GamePerPlayer<M> &getPerPlayer();
+  GamePerAudience<T> &getPerAudience();
 
-        bool inAudience;
-        GamePerAudience<T> audience; 
-        GamePerPlayer<M> players; 
-=======
-        Player(networking::Connection, const string&);
-        ~Player();
-        void make_choice(const string&);
-        networking::Connection getConnection() const;
-        int roundGetter();
-        void one_round_drop();
-        string getChoice() const;
-        int getId() const;
-        string getDisplayName() const;
-    private:
-        const networking::Connection connection;
-        int round;
-        string choice;
-        int id;
-        string displayName;
->>>>>>> 464115116f301a5b027831865f208f0a785170c2
+private:
+  networking::Connection connection;
+  string choice;
+  int id;
+
+  bool inAudience;
+  GamePerAudience<T> audience;
+  GamePerPlayer<M> players;
 };
--- game/include/game.h
+++ game/include/game.h
@@ -4,14 +4,15 @@
 
 #pragma once
 
-#include <string>
-#include <map>
-#include <vector>
 #include "GameConfiguration.h"
 #include "GameConstants.h"
-#include "GameVariables.h"
+#include "GamePerAudience.h"
 #include "GameRules.h"
+#include "GameVariables.h"
 #include "Player.h"
+#include <map>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -19,57 +20,45 @@ using namespace std;
 //     public:
 //         virtual ~Game() = default;
 //         virtual string determineWinner() = 0;
-//         virtual void processPlayerChoice(networking::Connection, const string&, networking::Server&) = 0;
-//         virtual void resetPlayerChoices() = 0;
-//         virtual string getGameName() const = 0;
-//         virtual void sendingMessage(networking::Server&, networking::Connection, const string&) const = 0;
+//         virtual void processPlayerChoice(networking::Connection, const
+//         string&, networking::Server&) = 0; virtual void resetPlayerChoices()
+//         = 0; virtual string getGameName() const = 0; virtual void
+//         sendingMessage(networking::Server&, networking::Connection, const
+//         string&) const = 0;
 // };
 
 class Game {
-    GameConfiguration configuration;
-    GameConstants constants;
-    GameVariables variables;
-    GameRules rules;
-    vector<Player> players;
+  GameConfiguration configuration;
+  GameConstants constants;
+  GameVariables variables;
+  GameRules rules;
+  vector<Player> players;
 
-    public:
-<<<<<<< HEAD
-        // Constructor (cannot be instantiated without all provided fields ie. no default constructor allowed)
-        Game(GameConfiguration& configuration, GameConstants& constants, GameVariables& variables, GamePerAudience& perAudience, GameRules & rules, vector<Player> players)
-            : configuration(configuration), constants(constants), variables(variables), perAudience(perAudience), rules(rules), players(players) {
-                //TODO: Perhaps deep copy Players vector?
-            }
+public:
+  // Constructor (cannot be instantiated without all provided fields ie. no
+  // default constructor allowed)
+  Game(GameConfiguration &configuration, GameConstants &constants,
+       GameVariables &variables, GamePerAudience &perAudience, GameRules &rules,
+       vector<Player> players)
+      : configuration(configuration), constants(constants),
+        variables(variables), perAudience(perAudience), rules(rules),
+        players(players) {
+    // TODO: Perhaps deep copy Players vector?
+  }
 
-        ~Game() = default;  // Default destructor
+  ~Game() = default; // Default destructor
 
-        // Getters for important game data
-        GameConfiguration& getConfiguration();
-        GameConstants& getConstants();
-        GameVariables& getVariables();
-        GameRules& getRules();
-        vector<Player>& getPlayers();
+  // Getters for important game data
+  GameConfiguration &getConfiguration();
+  GameConstants &getConstants();
+  GameVariables &getVariables();
+  GameRules &getRules();
+  vector<Player> &getPlayers();
 
-
-        // TODO: I feel like we should move this code to a Manager class. 
-        // void sendingMessage(networking::Server&, networking::Connection, const string&);
-        // void processPlayerChoice(networking::Connection, const string&, networking::Server&);
-        // void resetPlayerChoices();
+  // TODO: I feel like we should move this code to a Manager class.
+  // void sendingMessage(networking::Server&, networking::Connection, const
+  // string&); void processPlayerChoice(networking::Connection, const string&,
+  // networking::Server&); void resetPlayerChoices();
 };
 
 #endif
-=======
-        Game(string gameName) : gameName(gameName) {}
-        ~Game();
-        string determineWinner();
-        void processPlayerChoice(networking::Connection, const string&, networking::Server&);
-        void resetPlayerChoices(); 
-        string getGameName() const;
-        void sendingMessage(networking::Server&, networking::Connection, const string&) const;
-        void addPlayer(Player);
-        vector<Player> getPlayers() const;
-    private:
-        string gameName;
-        vector<Player> players;
-};
-
->>>>>>> 464115116f301a5b027831865f208f0a785170c2
